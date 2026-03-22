0984582e39c85bc0805e8a769ca2f246c798dcfd
Omnia771148 <omnia771148@gmail.com>
2026-03-22 17:23:56 +0530

    Added Delivery Boy native app custom sound and connection integration

--- src/app/data/page.js
+++ src/app/data/page.js
@@ -1,15 +1,34 @@
 
 import { Data as AmigoData } from '../AmigoNoshery/data';
 import { Data as vivaData } from '../vivafinedine/data';
-
-import {Data as MrhangoutData} from '../mrhangout/data';
-
-
+import { Data as MrhangoutData } from '../mrhangout/data';
+import { Data as ReddyData } from '../reddyfamilyrest/data';
+import { Data as AhaData } from '../ahakitchens/data';
+import { Data as BroStoryData } from '../brostory/data';
+import { Data as FunAndFoodData } from '../funandfood/data';
+import { Data as PRGrandData } from '../prgrand/data';
+import { Data as FoodLandData } from '../foodland/data';
+import { Data as TalimpuData } from '../talimpu/data';
+import { Data as TajDarbarData } from '../tajdarbar/data';
+import { Data as RuchivedhikaData } from '../ruchivedhika/data';
+import { Data as HindustanData } from '../hindustan/data';
+import { Data as LassyCornerData } from '../lassycorner/data';
+import { Data as Mandi9RData } from '../mandi9r/data';
 
 export const Data = [
     ...AmigoData,
     ...vivaData,
-
-    ...MrhangoutData
-
+    ...MrhangoutData,
+    ...ReddyData,
+    ...AhaData,
+    ...BroStoryData,
+    ...FunAndFoodData,
+    ...PRGrandData,
+    ...FoodLandData,
+    ...TalimpuData,
+    ...TajDarbarData,
+    ...RuchivedhikaData,
+    ...HindustanData,
+    ...LassyCornerData,
+    ...Mandi9RData
 ];
